52c624b7d1227eedecae57e56eb1c6eb95a859f2
Israel Rodrigo Ochoa Mejia <rodrigo.ochoa333@gmail.com>
2026-09-07 11:01:07 -0500

    Commit de prueba

---
+++ prueba.py
@@ -0,0 +1,11 @@
+def hola_mundo():
+    print("¡Hola, mundo!")
+
+# Para ejecutar la función:
+hola_mundo()
+
+def saludar_usuario(nombre):
+    return f"¡Hola, {nombre}!"
+
+# Para ejecutar la función:
+print(saludar_usuario("Carlos"))
